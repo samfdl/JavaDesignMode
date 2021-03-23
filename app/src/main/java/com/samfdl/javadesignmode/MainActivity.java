4f9d65b22c3f7a57fb317c1585c7ca4ce986c823
冯德林 <samfdl@163.com>
2021-03-23 15:56:14 +0800

    加入美国总统单例例子

--- app/src/main/java/com/samfdl/javadesignmode/MainActivity.java
+++ app/src/main/java/com/samfdl/javadesignmode/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.samfdl.javadesignmode.create.singleton.case1.SingletonLazy;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SingletonLazy.main();
     }
 }
